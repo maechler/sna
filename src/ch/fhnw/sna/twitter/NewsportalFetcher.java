3f1b80e8385d82912107d0e70d20e728a65b6614
unknown <christian@W10NB01.schneider.local>
2015-11-09 22:20:42 +0100

    ID fetcher & random chooser No data yet

--- src/ch/fhnw/sna/twitter/NewsportalFetcher.java
+++ src/ch/fhnw/sna/twitter/NewsportalFetcher.java
@@ -1,17 +1,25 @@
 package ch.fhnw.sna.twitter;
 
-import ch.fhnw.sna.twitter.model.HumanTwitterUser;
 import ch.fhnw.sna.twitter.model.NewsportalGraph;
 import ch.fhnw.sna.twitter.model.NewsportalTwitterUser;
 import twitter4j.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Set;
 
 public class NewsportalFetcher {
     private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(NewsportalFetcher.class);
     private Twitter twitter = new TwitterFactory().getInstance();
 
-    public NewsportalGraph fetch(ArrayList<String> newsportals) {
+    public NewsportalGraph fetch(ArrayList<String> newsportals) throws InterruptedException, IOException {
         NewsportalGraph graph = new NewsportalGraph();
 
         try {
@@ -35,19 +43,94 @@ public class NewsportalFetcher {
         }
     }
 
-    public void fetchHumans(ArrayList<String> newsportals, NewsportalGraph graph) throws TwitterException {
+    public void fetchHumans(ArrayList<String> newsportals, NewsportalGraph graph) throws TwitterException, InterruptedException, IOException {
 
-        long cursor = -1;
-        IDs ids;
-        Twitter twitter = new TwitterFactory().getInstance();
-        do {
-            ids = twitter.getFollowersIDs("tagi", cursor, -1);
+        for(String newsportal : newsportals)
+        {
+            
+            File f = new File("data/"+newsportal+".txt");
+            f.createNewFile();
+            
+            FileWriter writer = new FileWriter("data/"+newsportal+".txt", false);
+            
+            long cursor = -1;
+            IDs ids = null;
+            IDs ids2 = null;
+            Twitter twitter = new TwitterFactory().getInstance();
+            do {
+                try
+                {
+                    writer = new FileWriter("data/"+newsportal+".txt", true);
+                    ids2 = ids;
+                    ids = twitter.getFollowersIDs(newsportal, cursor, -1);
+                    LOG.info("Fetched ids " + ids.getIDs().length + " from "+newsportal);
+                    int i = 0;
+                    for(i = 0;i<ids.getIDs().length;++i)
+                    {
+                        writer.write(Objects.toString(ids.getIDs()[i])+"\r\n");
+                    }
+                    
+                    LOG.info(i+" ids written to /data/" + newsportal + ".txt");
+                  
+                    LOG.info("Sleeping...");
+                    Thread.sleep(65000);
+                    
+                }
+                catch(TwitterException e)
+                {
+                    ids = ids2;
+                    LOG.info(e.toString());
+                    Thread.sleep(60000);
+                }
+                finally
+                {
+                    writer.close();
+                }
+            } while ((cursor = ids.getNextCursor()) != 0);  
+            
+            
+            
+        }
 
-            LOG.info("Fetched ids " + ids.getIDs().length);
-            /*for (long id : ids.getIDs()) {
-                System.out.println("fetched id count " + id);
-            }*/
-        } while ((cursor = ids.getNextCursor()) != 0);
+    }
+    
+    public void pickRandomIDs(ArrayList<String> newsportals) throws IOException
+    {
+        for(String newsportal : newsportals)
+        {
+            // ID Datei lesen und in Array speichern
+            FileReader fileReader = new FileReader("data/"+newsportal+".txt");
 
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            ArrayList<String> lines = new ArrayList<String>();
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                lines.add(line);
+            }
+            bufferedReader.close();
+           
+            // Zufallszahlengenerator welche 5% der Abzahl IDs in einer eindeutigen Liste zurückgibt
+            Random rng = new Random(); // Ideally just create one instance globally
+            Set<Integer> generated = new LinkedHashSet<Integer>();
+            while (generated.size() < (lines.size()*0.05))
+            {
+                Integer next = rng.nextInt(lines.size()) + 1;
+                generated.add(next);
+            }
+         
+            // Neue Datei mit den zufällig gewählen 5% schreiben
+            File f = new File("data/"+newsportal+"_5percent.txt");
+            f.createNewFile();
+            
+            FileWriter writer = new FileWriter("data/"+newsportal+"_5percent.txt", false);
+            
+            for(int number : generated)
+            {
+                writer = new FileWriter("data/"+newsportal+"_5percent.txt", true);
+                writer.write(lines.get(number)+"\r\n");
+                writer.close();
+            }
+        }
     }
 }
--- src/ch/fhnw/sna/twitter/NewsportalFetcher_Main.java
+++ src/ch/fhnw/sna/twitter/NewsportalFetcher_Main.java
@@ -1,26 +1,30 @@
 package ch.fhnw.sna.twitter;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import ch.fhnw.sna.twitter.model.NewsportalGraph;
 
 public class NewsportalFetcher_Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, IOException {
         String FILE = "Newsportals.gexf";
 
         ArrayList<String> newsportals = new ArrayList<String>();
         
         // Newsportale
-        newsportals.add("NZZ");
         newsportals.add("tagi");
-        newsportals.add("Blickch");
         newsportals.add("watson_news");
-        newsportals.add("blickamabend");
-        newsportals.add("20min");
         newsportals.add("Weltwoche");
-        newsportals.add("SRF");
+        newsportals.add("blickamabend");
+        newsportals.add("Blickch");
+        newsportals.add("NZZ");
+        newsportals.add("20min");
+        //newsportals.add("SRF");
         
         NewsportalGraph graph = new NewsportalFetcher().fetch(newsportals);
-        new NewsportalGephiExport(FILE).export(graph);
+        
+        new NewsportalFetcher().pickRandomIDs(newsportals);
+        
+        //new NewsportalGephiExport(FILE).export(graph);
     }
 }
