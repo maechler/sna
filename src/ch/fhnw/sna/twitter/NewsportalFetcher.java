954dc9839d31b61bc71e243f9c2dfacc1534a6c3
Markus Mächler <markus.maechler@bithost.ch>
2015-11-09 16:03:31 +0100

    Adds TwitterAuth

--- src/ch/fhnw/sna/twitter/NewsportalFetcher.java
+++ src/ch/fhnw/sna/twitter/NewsportalFetcher.java
@@ -1,9 +1,30 @@
 package ch.fhnw.sna.twitter;
 
 import ch.fhnw.sna.twitter.model.NewsportalGraph;
+import twitter4j.*;
 
 public class NewsportalFetcher {
+    private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(NewsportalFetcher.class);
+
     public NewsportalGraph fetch() {
-        return new NewsportalGraph();
+        NewsportalGraph graph = new NewsportalGraph();
+
+        try {
+            long cursor = -1;
+            PagableResponseList<User> followers;
+            Twitter twitter = new TwitterFactory().getInstance();
+            do {
+                followers = twitter.getFollowersList("srf", cursor);
+                for (User follower : followers) {
+                    // TODO: Collect top 10 followers here
+                    System.out.println(follower.getName() + " has " + follower.getFollowersCount() + " follower(s)");
+                }
+            } while ((cursor = followers.getNextCursor()) != 0);
+        } catch (TwitterException e) {
+            LOG.error("Got TwitterException while fetching: {}", e.getErrorMessage());
+        }
+
+
+        return graph;
     }
 }
